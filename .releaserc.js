29f5d4f58785d89b8e30be7719ba67696268b4bb
David Lepaux <dalepaux@sqli.com>
2024-11-11 11:07:39 +0100

    fix(semantic-config): fix

--- .releaserc.js
+++ .releaserc.js
@@ -3,20 +3,15 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',   // Analyzes commit messages to decide version bump
     '@semantic-release/release-notes-generator', // Generates release notes
-    // Updates the changelog
-    [
-      "@semantic-release/changelog",
-      {
-        "changelogFile": "changelog.md"
-      }
-    ],
     '@semantic-release/npm'  // Update the package.json file
+    // Updates the changelog
+    ["@semantic-release/changelog", {
+      "changelogFile": "changelog.md"
+    }],
     // Commits changes (version, changelog)
-    [ 
-      "@semantic-release/git",
-      {
-        "assets": ["changelog.md"]
-      }
-    ],
+    ["@semantic-release/git", {
+      "assets": ["changelog.md", "docs", "package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
   ],
 };
